f9186249f1927530a54c063b32aa3e424698ba82
SahuSumanta <smsahu9@gmail.com>
2022-06-25 19:53:38 +0530

    sum of first 10 natural numbers

---
+++ Assignment/L2.java
@@ -0,0 +1,11 @@
+package Assignment;
+
+public class L2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
