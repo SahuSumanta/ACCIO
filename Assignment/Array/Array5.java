475d0af0fda8b7e0ab9369b70ead382026b17b6e
SahuSumanta <smsahu9@gmail.com>
2022-07-03 11:30:40 +0530

    2nd Largest from array

---
+++ Assignment/Array/Array5.java
@@ -0,0 +1,20 @@
+package Assignment.Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[n-2]);        
+    }
+}
