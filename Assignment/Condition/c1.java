08974e97166cc5a53cdb93ab78fcffb2510f2bfe
SahuSumanta <smsahu9@gmail.com>
2022-07-05 19:56:32 +0530

    Celsius to Fahrenheit

---
+++ Assignment/Condition/c1.java
@@ -0,0 +1,18 @@
+package Assignment.Condition;
+
+import java.util.*;
+
+
+public class c1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    
+    Scanner in = new Scanner(System.in);
+    int C = in.nextInt();
+		System.out.println(C * 9/5 + 32);
+
+        in.close();
+	}
+    
+}
