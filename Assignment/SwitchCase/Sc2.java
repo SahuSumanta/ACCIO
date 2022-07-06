d05e97e99e6181f4ec658dfea01301340ae86ac7
SahuSumanta <smsahu9@gmail.com>
2022-07-06 22:28:33 +0530

    CalculatenCr

---
+++ Assignment/SwitchCase/Sc2.java
@@ -0,0 +1,31 @@
+package Assignment.SwitchCase;
+
+import java.util.Scanner;
+
+/**
+ * CalculatenCr
+ */
+public class Sc2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int r = in.nextInt();
+        int diff = n-r;
+
+        long factn = 1, factr = 1, factdiff = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            factn *= i;
+        }
+
+        for (int i = 1; i <= r; i++) {
+            factr *= i;
+        }
+
+        for (int i = 1; i <= diff; i++) {
+            factdiff *= i;
+        }
+
+        System.out.println(factn/(factdiff * factr));
+    }
+}
