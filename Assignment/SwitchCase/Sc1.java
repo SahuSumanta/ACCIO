41f1bb8aa42da05adcde8ab2698a8bfdf9c3b301
SahuSumanta <smsahu9@gmail.com>
2022-07-06 22:13:52 +0530

    Factorial Of a Number

---
+++ Assignment/SwitchCase/Sc1.java
@@ -0,0 +1,113 @@
+package Assignment.SwitchCase;
+
+import java.util.Scanner;
+
+/**
+ * Factorial Of a Number
+ */
+public class Sc1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long fact = 1;
+
+        switch (n) {
+                case 1:
+                    for (int index = 1; index <= n; index++) {
+                        fact*=index;
+                    }
+                    System.out.println(fact);
+                    break;
+                case 2:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 3:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 4:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 5:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 6:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 7:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 8:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 9:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 10:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 11:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 12:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 13:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 14:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+                case 15:
+                for (int index = 1; index <= n; index++) {
+                    fact*=index;
+                }
+                System.out.println(fact);
+                break;
+            default:
+                System.out.println(fact);
+                break;
+        }
+
+        sc.close();
+    }
+}
