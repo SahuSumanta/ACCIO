0dd6bced9c773d17cbf9a6325489128597461c5f
SahuSumanta <smsahu9@gmail.com>
2022-07-12 14:55:34 +0530

    Recursive Fibonacci

---
+++ Assignment/Recursion/R2.java
@@ -0,0 +1,20 @@
+package Assignment.Recursion;
+
+import java.util.Scanner;
+
+public class R2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibo(n));
+        sc.close();
+    }
+
+    static int fibo(int n){
+        // if(n <= 0) return 0;
+        if(n == 1) return 0;
+        if(n == 2 ||n == 3) return 1;
+
+        return fibo(n-1)+fibo(n-2);
+    }
+}
